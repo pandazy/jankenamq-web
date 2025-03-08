7f922b6046bde6f156092c3b3d5a7393c39851e1
Martin Zheng <jszhengyq@gmail.com>
2025-03-07 22:33:10 -0800

    build: optimized the build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,19 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		minify: 'esbuild',
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					mui: ['@mui/material', '@mui/icons-material'],
+					react: ['react', 'react-dom'],
+					router: ['react-router-dom'],
+					query: ['@tanstack/react-query'],
+				},
+			},
+		},
+	},
 	resolve: {
 		alias: {
 			'~': resolve(__dirname, './src'),
